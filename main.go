693ef88ebdbb8fc3b1e6e5df5164bc19c892747c
Thiago Mallon <jamesmallondev@gmail.com>
2019-07-02 11:48:22 -0700

    Cleaning

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	log "GoAuthentication/libs/logger"
 	. "GoAuthentication/routers"
 	"net/http"
 )
@@ -10,8 +9,6 @@ func main() {
 	LoadRoutes()
 	LoadAuthRoutes()
 	LoadAdminRoutes()
-
-	log.Write("Error", "Venom is the new Klyntar's emperor! Heil Venom!", log.Trace())
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
 	http.Handle("/libs/", http.StripPrefix("/libs/", http.FileServer(http.Dir("node_modules"))))
